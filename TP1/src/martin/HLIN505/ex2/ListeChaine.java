1bfaeadfc7ea1acd5df086f9c9f6120087540a0c
Martin <martin.sanchez.iut@gmail.com>
2020-09-21 10:28:40 +0200

    fin tp1

--- TP1/src/martin/HLIN505/ex2/ListeChaine.java
+++ TP1/src/martin/HLIN505/ex2/ListeChaine.java
@@ -6,20 +6,49 @@ public class ListeChaine {
 	private Node racine;
 	
 	
+	public ListeChaine(int val) {
+		racine = new Node(val) ;
+	}
+	
+	
 	public Node getNext() {
 		return racine.next;
 	}
 	
+	
 	public int size() {
 		int size = 0;
+		
 		Node next = racine;
-		while(next.)
+		while(next != null) {
+			next = next.next ;
+			size++;
+		}
 		
 		return size;
 	}
 	
+	public String toString() {
+		StringBuilder sb = new StringBuilder() ;
+		
+		sb.append("Size = " + size() + " |") ;
+		
+		Node next = racine;
+		while(next != null) {
+			sb.append(next.value + " - ") ;
+			next = next.next ;
+		}	
+		return sb.toString();
+	}
 	
-	
+	public void addToHead(int nouveau) {
+		Node cache = racine ;
+		
+		racine = new Node(nouveau) ;
+		racine.next = cache;
+		
+		
+	}
 	
 	
 	
---
+++ TP1/src/martin/HLIN505/ex2/main.java
@@ -0,0 +1,20 @@
+package martin.HLIN505.ex2;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		ListeChaine lc = new ListeChaine(1) ;
+		System.out.println(lc.toString());
+		lc.addToHead(2);
+		System.out.println(lc.toString());
+		lc.addToHead(3);
+		lc.addToHead(4);
+		lc.addToHead(5);
+		lc.addToHead(6);
+		lc.addToHead(7);
+		System.out.println(lc.toString());
+
+
+	}
+}
